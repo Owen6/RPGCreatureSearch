dcbbeda8526419be5a303e3d04d8d339fd30b56d
Owen6 <owen.box2@gmail.com>
2025-05-06 09:22:30 -0700

    Completed project styling and logic. Filled out README. Completed Certification.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # RPGCreatureSearch
-App that will search for a creature, given a name or id, through data that is retrieved from freeCodeCamp's RPG Creature API.
+
+RPG Creature Stat Search Application
+FreeCodeCamp: JavaScript Algorithms and Data Structures Certification - Project 5
+
+Final Project in the certification program.
+
+Description:
+This project is an application that utilizes the provided FreeCodeCamp RPG Creature API to search and pull op the individual stats and attributes of a valid creature within the API either by Creature ID or Creature Name. If the value entered into the input field represents either a valid creature ID or valid creature name, the program will try to fetch the related data from the API and load it to the UI. If the value entered into the input field is not valid, the application will catch the error when trying to fetch the information from the API and the user will be alerted that no creature exists within the data. Indexing for Creature IDs starts at 1 and increments by 1 for what is inferred to be ease of use.
+
+API:"https://rpg-creature-api.freecodecamp.rocks/api/creature/{creature-name/creature-id}"
+    -{creature-name/creature-id} should be replaced with either a valid creature name or id.
+    -Example name: Pyrolynx -> "https://rpg-creature-api.freecodecamp.rocks/api/creature/Pyrolynx"
+    -Example ID: 1 -> "https://rpg-creature-api.freecodecamp.rocks/api/creature/1"
+
+Project Files:
+index.html
+styles.css
+script.js
+project5Instructions.png
+project_5_checks.png
+
+How it Works:
+1. Enter a value into the input field.
+2. Click the "Search" Button or hit Enter on your keyboard
+3. The algorithm will display the matching creature's stats to the user or will alert them that no creature in the system matches the given input.
+
+Technologies:
+1. HTML5
+2. CSS3
+3. JavaScript(ES6)
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//const validCreatureURL = "https://rpg-creature-api.freecodecamp.rocks/api/creatures";
-//https://rpg-creature-api.freecodecamp.rocks/api/creature/{name-or-id}
 const creatureInfoURL = "https://rpg-creature-api.freecodecamp.rocks/api/creature/";
 
 const searchInput = document.getElementById("search-input");
@@ -9,30 +7,27 @@ const creatureSpanMap = {};
 let creatures = [];
 let loaded = false;
 
-
-//console.log(creatureStatsSpans);
-
 //set creatureSpansMap so keys are id's instead of numbers for better accessibility.
 creatureStatsSpans.forEach((span) => {
-  //console.log(span.id);
-  //console.log(span.innerText);
   if(span.id){
     creatureSpanMap[span.id] = span;
   }
 })
 
-//console.log(creatureSpanMap)
-
 window.onload = () => {
-  //creatures = await fetchData();
   searchBtn.addEventListener("click", searchCreatures);
+  searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      console.log('test');
+      searchCreatures();
+    }
+  });
 }
 
 const fetchData = async (input) => {
   try {
     const res = await fetch(`${creatureInfoURL}${input}`);
     const data = await res.json();
-    //console.log(data);
     return data;
   } catch (err) {
     console.log(err);
@@ -51,16 +46,12 @@ const clearStats = () => {
 }
 
 const loadData = async (data) => {
-  
-  //const data = await fetchData(`${creatureInfoURL}${obj.id}`);
-  //console.log(data);
-
   creatureSpanMap["creature-name"].textContent = data.name.toUpperCase();
   creatureSpanMap["creature-id"].textContent = `#${data.id}`;
-  creatureSpanMap["weight"].textContent = `Weight: ${data.weight}`;
-  creatureSpanMap["height"].textContent = `Height: ${data.height}`;
+  creatureSpanMap["weight"].textContent = `${data.weight}`;
+  creatureSpanMap["height"].textContent = `${data.height}`;
   data.types.forEach((type) => {
-    creatureSpanMap["types"].innerHTML += `<div>${type.name.toUpperCase()}</div>`;
+    creatureSpanMap["types"].innerHTML += `<div class="type ${type.name.toLowerCase()}">${type.name.toUpperCase()}</div>`;
   })
   data.stats.forEach((stat) => {
     creatureSpanMap[stat.name].textContent = stat.base_stat;
@@ -70,25 +61,9 @@ const loadData = async (data) => {
 
 const searchCreatures = async () => {
   clearStats();
-  //const numInput = parseInt(input);
-
   const data = await fetchData(searchInput.value);
   if(data) {
     loadData(data);
   }
-  /**
-  let notFound = true;
-  //only use of creatures
-  for(const obj of creatures){
-    if(searchInput.value === obj.name || parseInt(searchInput.value) === obj.id){
-      notFound = false;
-      loadData(obj);
-    }
-  }
-  if(notFound){
-    clearStats();
-    alert("Creature not found");
-  }
-  */
 }
 
